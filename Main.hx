2a21d7e3fadbea9d0e5203226f672c68919e2eba
David Stephenson <davidystephenson@gmail.com>
2012-12-21 11:32:11 -0500

    fixed tabbing

--- Main.hx
+++ Main.hx
@@ -3,17 +3,17 @@ import flash.display.MovieClip;
 
 class Main extends MovieClip {
 
-	public var console:ConsoleSender();
-	public var canvas:MovieClip;
+    public var console:ConsoleSender();
+    public var canvas:MovieClip;
 
-	public function new() {
-		super();
-		
-		console = new ConsoleSender();
-		canvas = flash.Lib.current;
-	} // public function new()
+    public function new() {
+        super();
+        
+        console = new ConsoleSender();
+        canvas = flash.Lib.current;
+    } // public function new()
 
-	static function main() {
-		Lib.current.stage.addChild(new Main());
-	} // static function main()
+    static function main() {
+        Lib.current.stage.addChild(new Main());
+    } // static function main()
 } // class Main
